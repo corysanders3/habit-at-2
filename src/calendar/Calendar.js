4944405382c7cd88a4f5d815dc4fe7037a6cdfbb
David Kwon <112133897+dkwon1223@users.noreply.github.com>
2024-05-13 13:48:36 -0600

    Import calendar library and plugins

--- src/calendar/Calendar.js
+++ src/calendar/Calendar.js
@@ -1,11 +1,19 @@
 import './Calendar.css';
+import Fullcalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction"
 
 function Calendar() {
 
     return (
-        <>
-        
-        </>
+        <section className='calendar-page'>
+            <Fullcalendar 
+            className="calendar"
+            plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+            initialView={"dayGridMonth"}
+            />
+        </section>
     )
 }
 
