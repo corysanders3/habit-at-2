99e8c6c6d55489f14fd2048c1e9ffbce9d31a249
Cory Sanders <corysanders3@yahoo.com>
2024-05-14 17:18:21 -0600

    dummy commit for circle ci

--- cypress/e2e/dashboard_spec.cy.js
+++ cypress/e2e/dashboard_spec.cy.js
@@ -3,5 +3,6 @@ describe('test test', () => {
     cy.visit('http://localhost:3000')
     cy.expect(true).to.equal(true)
     cy.get('h1').contains('hello')
+    cy.get('h2').contains('hi')
   })
 })
