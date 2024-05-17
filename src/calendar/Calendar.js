a54a648c3f7c5cb71b96e4eaa4975bd75c97bc9f
David Kwon <112133897+dkwon1223@users.noreply.github.com>
2024-05-17 14:10:31 -0600

    Remove console log and reformat calendar component

--- src/calendar/Calendar.js
+++ src/calendar/Calendar.js
@@ -5,34 +5,33 @@ import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import { mockEvents } from "../mockData/mockHabits";
 
-
 function Calendar() {
-
-    function convertFrequency(frequency) {
-        const frequencyStatus = Object.values(frequency);
-        const result = frequencyStatus.reduce((arr, status, index) => {
-            if(status) {
-                if(index === 6) {
-                    arr.push(0);
-                } else {
-                    arr.push(index + 1);
-                }
-            }
-            return arr;
-        }, [])
-        return result;
-    }
+  function convertFrequency(frequency) {
+    const frequencyStatus = Object.values(frequency);
+    const result = frequencyStatus.reduce((arr, status, index) => {
+      if (status) {
+        if (index === 6) {
+          arr.push(0);
+        } else {
+          arr.push(index + 1);
+        }
+      }
+      return arr;
+    }, []);
+    return result;
+  }
 
   const parsedEvents = mockEvents.map((event) => {
     return {
       title: event.attributes.name,
       startRecur: event.attributes.start_datetime.slice(0, 10),
       endRecur: event.attributes.end_datetime.slice(0, 10),
-      daysOfWeek: event.attributes.frequency === "weekly" ? convertFrequency(event.attributes.custom_frequency) : null
-     };
+      daysOfWeek:
+        event.attributes.frequency === "weekly"
+          ? convertFrequency(event.attributes.custom_frequency)
+          : null,
+    };
   });
-
-  console.log(parsedEvents);
 
   return (
     <section className="calendar-page">
