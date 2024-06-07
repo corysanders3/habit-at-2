56186138cd1b38dbfa8832faf12717124efb7fa1
David Kwon <112133897+dkwon1223@users.noreply.github.com>
2024-06-07 09:03:58 -0600

    Fix bug with component suspension

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import App from './app/App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { Suspense } from 'react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <React.StrictMode>
-      <App />
+      <Suspense fallback={<div>Loading... </div>}>
+        <App />
+      </Suspense>
     </React.StrictMode>
   </BrowserRouter>
 );
